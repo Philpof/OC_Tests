ab05d4a8754b17ea22e490fe9d7fd1f08c42d585
Philippe PERECHODOV <philippe.perechodov@free.fr>
2020-12-19 12:39:26 +0100

    Test 1 : énoncé

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Philippe PERECHODOV">
+    <title>Tests - OCR</title>
+
+</head>
+
+<body>
+    
+<?php
+
+    $number = 4;
+
+    function parseToRoman(int $number) {
+        
+
+
+    };
+
+    // parseToRoman(4); // === "IV"
+    // parseToRoman(37); // === "XXXVII"
+    // parseToRoman(143); // === "CXLIII"
+    // parseToRoman(1234); // === "MCCXXXIV"
+
+?>
+
+</body>
+</html>
