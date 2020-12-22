5b44392880a696d33ef4889cc1be49279fea79bf
Philippe PERECHODOV <philippe.perechodov@free.fr>
2020-12-22 21:08:51 +0100

    const ROMAIN (array)

--- index.php
+++ index.php
@@ -10,74 +10,67 @@
 
 <body>
     <p>
-<?php
+        <?php
 
-// Résultats attendus
-    // parseToRoman(4); // === "IV"
-    // parseToRoman(37); // === "XXXVII"
-    // parseToRoman(143); // === "CXLIII"
-    // parseToRoman(1234); // === "MCCXXXIV"
+        // Résultats attendus
+            // parseToRoman(4); // === "IV"
+            // parseToRoman(37); // === "XXXVII"
+            // parseToRoman(143); // === "CXLIII"
+            // parseToRoman(1234); // === "MCCXXXIV"
 
-    parseToRoman(1234);
+        // Ma réponse
+            parseToRoman(1234);
 
-// Ma réponse
-    function parseToRoman(int $number) {
-        
-        $I = "I";
-        $V = "V";
-        $X = "X";
-        $L = "L";
-        $C = "C";
-        $D = "D";
-        $M = "M";
+            function parseToRoman(int $number) {
 
-        if (substr($number, 0, -3) !== null && substr($number, 0, -3) > 0) {
-            $mil = str_repeat($M, (substr($number, 0, -3)));
-        } else {
-            $mil = null;
-        }
+                define('ROMAIN', array('I', 'V', 'X', 'L', 'C', 'D', 'M'));
 
-        if (substr($number, -3, 1) !== null && substr($number, 0, -2) > 0) {
-            if (substr($number, -3, 1) < 4) {
-                $cent = str_repeat($C, (substr($number, -3, 1)));
-            } elseif (substr($number, -3, 1) == 4) {
-                $cent = $C . $D;
-            } else {
-                $cent = $D . str_repeat($C, (substr($number, -3, 1)) - 5);
-            }
-        } else {
-            $cent = null;
-        }
-        
-        if (substr($number, -2, 1) !== null && substr($number, 0, -1) > 0) {
-            if (substr($number, -2, 1) < 4) {
-                $diz = str_repeat($X, (substr($number, -2, 1)));
-            } elseif (substr($number, -2, 1) == 4) {
-                $diz = $X . $L;
-            } else {
-                $diz = $L . str_repeat($C, (substr($number, -2, 1)) - 5);
-            }
-        } else {
-            $diz = null;
-        }
+                if (substr($number, 0, -3) !== null && substr($number, 0, -3) > 0) {
+                    $mil = str_repeat(ROMAIN[6], (substr($number, 0, -3)));
+                } else {
+                    $mil = null;
+                }
 
-        if (substr($number, -1, 1) !== null && $number > 0) {
-            if (substr($number, -1, 1) < 4) {
-                $unit = str_repeat($I, (substr($number, -1, 1)));
-            } elseif (substr($number, -1, 1) == 4) {
-                $unit = $I . $V;
-            } else {
-                $unit = $V . str_repeat($I, (substr($number, -1, 1)) - 5);
-            }
-        } else {
-            $unit = null;
-        }
-        
-        echo $result = $mil . $cent . $diz . $unit;
-    };
+                if (substr($number, -3, 1) !== null && substr($number, 0, -2) > 0) {
+                    if (substr($number, -3, 1) < 4) {
+                        $cent = str_repeat(ROMAIN[4], (substr($number, -3, 1)));
+                    } elseif (substr($number, -3, 1) == 4) {
+                        $cent = ROMAIN[4] . ROMAIN[5];
+                    } else {
+                        $cent = ROMAIN[5] . str_repeat(ROMAIN[4], (substr($number, -3, 1)) - 5);
+                    }
+                } else {
+                    $cent = null;
+                }
+                
+                if (substr($number, -2, 1) !== null && substr($number, 0, -1) > 0) {
+                    if (substr($number, -2, 1) < 4) {
+                        $diz = str_repeat(ROMAIN[2], (substr($number, -2, 1)));
+                    } elseif (substr($number, -2, 1) == 4) {
+                        $diz = ROMAIN[2] . ROMAIN[3];
+                    } else {
+                        $diz = ROMAIN[3] . str_repeat(ROMAIN[4], (substr($number, -2, 1)) - 5);
+                    }
+                } else {
+                    $diz = null;
+                }
 
-?>
-</p>
+                if (substr($number, -1, 1) !== null && $number > 0) {
+                    if (substr($number, -1, 1) < 4) {
+                        $unit = str_repeat(ROMAIN[0], (substr($number, -1, 1)));
+                    } elseif (substr($number, -1, 1) == 4) {
+                        $unit = ROMAIN[0] . ROMAIN[1];
+                    } else {
+                        $unit = ROMAIN[1] . str_repeat(ROMAIN[0], (substr($number, -1, 1)) - 5);
+                    }
+                } else {
+                    $unit = null;
+                }
+                
+                echo $mil . $cent . $diz . $unit;
+            };
+        ?>
+    </p>
 
 </body>
 </html>
